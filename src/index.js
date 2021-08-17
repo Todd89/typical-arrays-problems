759f092abf8317625df721cc26f35fb6b393a72c
Todd89 <alex-89fish@tut.by>
2021-08-17 14:14:09 +0300

    feat: code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
-}
 
+  let min = array[0];
+  if (typeof array === 'undefined' || array.length === 0 || typeof array === 'string' || array[0] === 0 || array [0] === '0') {
+    return 0;
+  }
+  for (i = 1; i < array.length; ++i) {
+   if (typeof array[i] === 'string' || array [i] === '0') {
+     return 0;
+   }
+    if (array[i] < min) min = array[i];
+   }
+
+   return min;
+}
 exports.max = function max (array) {
-  return 0;
+  let max = array[0];
+  if (typeof array === 'undefined' || array.length === 0) {
+    return 0;
+  }
+  for (i = 1; i < array.length; ++i) {
+   
+    if (array[i] > max) max = array[i];
+   }
+
+   return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  let avg = 0;
+  if (typeof array === 'undefined' || array.length === 0) {
+    return 0;
+  }
+ for (i = 0; i < array.length; ++i) {
+	sum += array [i];
+  }
+  avg = sum / array.length;
+  return avg;
 }
+
+
